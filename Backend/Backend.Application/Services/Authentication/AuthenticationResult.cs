14c6aa5933a3dc93535264c59b394cb661ac95e2
Thái Dương <105373894+ThaiDuong2002@users.noreply.github.com>
2025-03-25 00:18:12 +0700

    03 - Authentication Service in Application layer

---
+++ Backend/Backend.Application/Services/Authentication/AuthenticationResult.cs
@@ -0,0 +1,10 @@
+namespace Backend.Application.Services.Authentication
+{
+    public record AuthenticationResult(
+        Guid Id,
+        string FirstName,
+        string LastName,
+        string Email,
+        string Token
+    );
+}
