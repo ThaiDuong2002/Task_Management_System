96034dc8696ee9d1ee8a8088f2dbb89d2aee0f8b
Thái Dương <105373894+ThaiDuong2002@users.noreply.github.com>
2025-04-11 11:19:15 +0700

    59 - Frontend: Fix Layout bug

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,17 +1,15 @@
 <script setup lang="ts">
-import { MainSidebar } from "@/components/customs/MainSidebar";
 import { DefaultLayout, MainLayout } from "@/components/layouts";
-import { SidebarProvider } from "@/components/ui/sidebar";
 import { computed } from "vue";
 import { useRoute } from "vue-router";
 
 const route = useRoute();
 
 const currentLayout = computed(() => {
-  if (route.meta.layout === "default") {
-    return DefaultLayout;
-  } else {
+  if (route.meta.layout === "main") {
     return MainLayout;
+  } else {
+    return DefaultLayout;
   }
 });
 </script>
